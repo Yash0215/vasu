b5198fc1567706fd23a9c6034b6db6572df6867f
yashchawada <yash15.chawada@gmail.com>
2023-06-22 13:51:01 +0530

    Dup Number Optimized class add

--- src/main/java/org/problems/TwoNumberSumProblem.java
+++ src/main/java/org/problems/TwoNumberSumProblem.java
@@ -1,5 +1,7 @@
 package org.problems;
 
+import java.util.HashSet;
+
 public class TwoNumberSumProblem {
 
     public static void main(String[] args) {
@@ -11,6 +13,31 @@ public class TwoNumberSumProblem {
         int[] numbers = {1, 2, 3, 4, 5};
         int target = 7;
 
+        int currentNumber;
+//        for(int i=0; i<numbers.length; i++) {
+//            currentNumber = numbers[i];
+//            for(int j=i+1; j<numbers.length; j++) {
+//                if (currentNumber + numbers[j]  == target) {  // CHECKING FOR DUPLICITY
+//                    System.out.println(currentNumber + " , " + numbers[j]);
+//                }
+//            }
+//        }
+
+      //  array = [1, 2, 3, 4, 5]    search (5) = check each and every number (for loop )- time complexity = O(n)
+      //  hashset = [1, 2, 3, 4, 5]  search(3) = it already knows where to look (contains(num))- time complexity = O(1)
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<numbers.length; i++) {
+            set.add(numbers[i]);
+        }
+
+        for(int i=0; i<numbers.length; i++) {
+            currentNumber = numbers[i];
+            if(set.contains(target - currentNumber)) {
+                System.out.println(currentNumber + " , " + (target - currentNumber));
+                System.exit(0);
+            }
+        }
 
 
     }
